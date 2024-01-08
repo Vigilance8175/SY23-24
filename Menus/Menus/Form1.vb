03549584c628948f89d1617bc28c82e18089d32a
Austin Axlund <amaxl934@learner.dvusd.org>
2024-01-08 14:01:51 -0700

    Added Menu

--- Menus/Menus/Form1.vb
+++ Menus/Menus/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
 
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        TextBox1.Clear()
+    End Sub
 End Class
