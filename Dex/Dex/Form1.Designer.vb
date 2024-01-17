92794e2a5a895629011c06aff6a804d92f90e812
Austin Axlund <amaxl934@learner.dvusd.org>
2024-01-17 14:09:37 -0700

    Added Button Navigation

--- Dex/Dex/Form1.Designer.vb
+++ Dex/Dex/Form1.Designer.vb
@@ -38,6 +38,10 @@ Partial Class Form1
         Me.Field4 = New System.Windows.Forms.TextBox()
         Me.Field5 = New System.Windows.Forms.TextBox()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.FirstButton = New System.Windows.Forms.Button()
+        Me.PrevButton = New System.Windows.Forms.Button()
+        Me.NextButton = New System.Windows.Forms.Button()
+        Me.LastButton = New System.Windows.Forms.Button()
         Me.MenuStrip1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -165,11 +169,51 @@ Partial Class Form1
         '
         Me.OpenFileDialog1.FileName = "OpenFileDialog1"
         '
+        'FirstButton
+        '
+        Me.FirstButton.Location = New System.Drawing.Point(202, 387)
+        Me.FirstButton.Name = "FirstButton"
+        Me.FirstButton.Size = New System.Drawing.Size(94, 29)
+        Me.FirstButton.TabIndex = 12
+        Me.FirstButton.Text = "First"
+        Me.FirstButton.UseVisualStyleBackColor = True
+        '
+        'PrevButton
+        '
+        Me.PrevButton.Location = New System.Drawing.Point(301, 387)
+        Me.PrevButton.Name = "PrevButton"
+        Me.PrevButton.Size = New System.Drawing.Size(94, 29)
+        Me.PrevButton.TabIndex = 13
+        Me.PrevButton.Text = "Previous"
+        Me.PrevButton.UseVisualStyleBackColor = True
+        '
+        'NextButton
+        '
+        Me.NextButton.Location = New System.Drawing.Point(400, 387)
+        Me.NextButton.Name = "NextButton"
+        Me.NextButton.Size = New System.Drawing.Size(94, 29)
+        Me.NextButton.TabIndex = 14
+        Me.NextButton.Text = "Next"
+        Me.NextButton.UseVisualStyleBackColor = True
+        '
+        'LastButton
+        '
+        Me.LastButton.Location = New System.Drawing.Point(499, 387)
+        Me.LastButton.Name = "LastButton"
+        Me.LastButton.Size = New System.Drawing.Size(94, 29)
+        Me.LastButton.TabIndex = 15
+        Me.LastButton.Text = "Last"
+        Me.LastButton.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LastButton)
+        Me.Controls.Add(Me.NextButton)
+        Me.Controls.Add(Me.PrevButton)
+        Me.Controls.Add(Me.FirstButton)
         Me.Controls.Add(Me.Field5)
         Me.Controls.Add(Me.Field4)
         Me.Controls.Add(Me.Field3)
@@ -209,4 +253,8 @@ Partial Class Form1
     Friend WithEvents Field4 As TextBox
     Friend WithEvents Field5 As TextBox
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents FirstButton As Button
+    Friend WithEvents PrevButton As Button
+    Friend WithEvents NextButton As Button
+    Friend WithEvents LastButton As Button
 End Class
--- Dex/Dex/Form1.vb
+++ Dex/Dex/Form1.vb
@@ -1,7 +1,8 @@
 ﻿Imports System.IO
-
 Public Class Form1
     Dim records(50) As String
+    Dim count As Integer
+    Dim current As Integer
     Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
         Field1.Text = ""
         Field2.Text = ""
@@ -28,28 +29,54 @@ Public Class Form1
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
         OpenFileDialog1.ShowDialog()
     End Sub
-
     Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
         PictureBox1.Load(OpenFileDialog1.FileName)
     End Sub
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         If IO.File.Exists("data.txt") Then
             Dim inFile As New IO.StreamReader("data.txt")
-            records(0) = inFile.ReadLine
+            While Not inFile.EndOfStream
+                records(count) = inFile.ReadLine
+                count = count + 1
+            End While
             inFile.Close()
             showrecord(0)
         End If
     End Sub
     Public Sub showrecord(index As Integer)
         Dim fields() As String
-        fields = records(index).Split("|")
-        Field1.Text = fields(0)
-        Field2.Text = fields(1)
-        Field3.Text = fields(2)
-        Field4.Text = fields(3)
-        Field5.Text = fields(4)
-        If File.Exists(fields(5)) Then
-            PictureBox1.Load(fields(5))
+        If records(index) <> Nothing Then
+            fields = records(index).Split("|")
+            Field1.Text = fields(0)
+            Field2.Text = fields(1)
+            Field3.Text = fields(2)
+            Field4.Text = fields(3)
+            Field5.Text = fields(4)
+            If File.Exists(fields(5)) Then
+                PictureBox1.Load(fields(5))
+            End If
+        End If
+    End Sub
+    Private Sub FirstButton_Click(sender As Object, e As EventArgs) Handles FirstButton.Click
+        current = -0
+        showrecord(current)
+    End Sub
+    Private Sub LastButton_Click(sender As Object, e As EventArgs) Handles LastButton.Click
+        current = count - 1
+        showrecord(current)
+    End Sub
+    Private Sub PrevButton_Click(sender As Object, e As EventArgs) Handles PrevButton.Click
+        If current > 0 Then
+            current = current - 1
+            showrecord(current)
+        End If
+    End Sub
+    Private Sub NextButton_Click(sender As Object, e As EventArgs) Handles NextButton.Click
+        If current < count - 1 Then
+            current = current + 1
+            showrecord(current)
         End If
     End Sub
 End Class
+
+
