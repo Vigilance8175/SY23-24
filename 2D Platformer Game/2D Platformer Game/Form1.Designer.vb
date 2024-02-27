eb464b243462266a78015844eb093f660a8b5432
Austin Axlund <amaxl934@learner.dvusd.org>
2024-02-27 14:00:42 -0700

    2D Platformer Created

---
+++ 2D Platformer Game/2D Platformer Game/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frm2Dplatformer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.picPlayer = New System.Windows.Forms.PictureBox()
+        Me.picAir = New System.Windows.Forms.PictureBox()
+        Me.picGround = New System.Windows.Forms.PictureBox()
+        Me.tmrRight = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrLeft = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrUp = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrGameLogic = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrGravity = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        CType(Me.picPlayer, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picAir, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picGround, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picPlayer
+        '
+        Me.picPlayer.BackColor = System.Drawing.Color.Purple
+        Me.picPlayer.Location = New System.Drawing.Point(-2, 407)
+        Me.picPlayer.Name = "picPlayer"
+        Me.picPlayer.Size = New System.Drawing.Size(36, 30)
+        Me.picPlayer.TabIndex = 0
+        Me.picPlayer.TabStop = False
+        '
+        'picAir
+        '
+        Me.picAir.Location = New System.Drawing.Point(-2, -3)
+        Me.picAir.Name = "picAir"
+        Me.picAir.Size = New System.Drawing.Size(844, 401)
+        Me.picAir.TabIndex = 1
+        Me.picAir.TabStop = False
+        '
+        'picGround
+        '
+        Me.picGround.BackColor = System.Drawing.Color.SpringGreen
+        Me.picGround.Location = New System.Drawing.Point(-2, 443)
+        Me.picGround.Name = "picGround"
+        Me.picGround.Size = New System.Drawing.Size(844, 50)
+        Me.picGround.TabIndex = 2
+        Me.picGround.TabStop = False
+        '
+        'tmrRight
+        '
+        Me.tmrRight.Interval = 20
+        '
+        'tmrLeft
+        '
+        Me.tmrLeft.Interval = 20
+        '
+        'tmrUp
+        '
+        Me.tmrUp.Interval = 20
+        '
+        'tmrGameLogic
+        '
+        Me.tmrGameLogic.Interval = 20
+        '
+        'tmrGravity
+        '
+        Me.tmrGravity.Interval = 20
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.IndianRed
+        Me.PictureBox1.Location = New System.Drawing.Point(461, 296)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(117, 20)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Tag = "bound"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.IndianRed
+        Me.PictureBox2.Location = New System.Drawing.Point(143, 350)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(117, 20)
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Tag = "bound"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.IndianRed
+        Me.PictureBox3.Location = New System.Drawing.Point(233, 159)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(117, 20)
+        Me.PictureBox3.TabIndex = 5
+        Me.PictureBox3.TabStop = False
+        Me.PictureBox3.Tag = "bound"
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.IndianRed
+        Me.PictureBox4.Location = New System.Drawing.Point(568, 189)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(117, 20)
+        Me.PictureBox4.TabIndex = 6
+        Me.PictureBox4.TabStop = False
+        Me.PictureBox4.Tag = "bound"
+        '
+        'frm2Dplatformer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SkyBlue
+        Me.ClientSize = New System.Drawing.Size(839, 494)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.picGround)
+        Me.Controls.Add(Me.picPlayer)
+        Me.Controls.Add(Me.picAir)
+        Me.Name = "frm2Dplatformer"
+        Me.Text = "Form1"
+        CType(Me.picPlayer, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picAir, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picGround, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents picPlayer As PictureBox
+    Friend WithEvents picAir As PictureBox
+    Friend WithEvents picGround As PictureBox
+    Friend WithEvents tmrRight As Timer
+    Friend WithEvents tmrLeft As Timer
+    Friend WithEvents tmrUp As Timer
+    Friend WithEvents tmrGameLogic As Timer
+    Friend WithEvents tmrGravity As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+End Class
---
+++ 2D Platformer Game/2D Platformer Game/Form1.vb
@@ -0,0 +1,70 @@
+﻿Imports System.Security.Cryptography
+
+Public Class frm2Dplatformer
+    Dim moveSpeed As Integer = 15
+    Dim isJumping As Boolean
+    Private Sub frm2DPlatformer_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyCode
+            Case Keys.Right
+                tmrRight.Start()
+            Case Keys.Left
+                tmrLeft.Start()
+            Case Keys.Up
+                tmrUp.Start()
+                isJumping = True
+        End Select
+    End Sub
+
+    Private Sub tmrRight_Tick(sender As Object, e As EventArgs) Handles tmrRight.Tick
+        picPlayer.Left += moveSpeed
+    End Sub
+
+    Private Sub frm2Dplatformer_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+        Select Case e.KeyCode
+            Case Keys.Right
+                tmrRight.Stop()
+            Case Keys.Left
+                tmrLeft.Stop()
+            Case Keys.Up
+                tmrUp.Stop()
+                isJumping = False
+        End Select
+    End Sub
+
+    Private Sub tmrLeft_Tick(sender As Object, e As EventArgs) Handles tmrLeft.Tick
+        picPlayer.Left -= moveSpeed
+    End Sub
+
+    Private Sub tmrUp_Tick(sender As Object, e As EventArgs) Handles tmrUp.Tick
+        picPlayer.Top -= moveSpeed
+    End Sub
+
+    Private Sub frm2Dplatformer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tmrGameLogic.Start()
+    End Sub
+
+    Private Sub tmrGameLogic_Tick(sender As Object, e As EventArgs) Handles tmrGameLogic.Tick
+        If picPlayer.Bounds.IntersectsWith(picAir.Bounds) Then
+            If isJumping = False Then
+                tmrGravity.Start()
+            End If
+        ElseIf picPlayer.Bounds.IntersectsWith(picGround.Bounds) Then
+            tmrGravity.Stop()
+        End If
+
+        For Each b As Control In Me.Controls
+            If TypeOf b Is PictureBox Then
+                If b.Tag = "bound" Then
+                    If picPlayer.Bounds.IntersectsWith(b.Bounds) Then
+                        tmrGravity.Stop()
+                    End If
+                End If
+            End If
+        Next
+
+    End Sub
+
+    Private Sub tmrGravity_Tick(sender As Object, e As EventArgs) Handles tmrGravity.Tick
+        picPlayer.Top += moveSpeed
+    End Sub
+End Class
