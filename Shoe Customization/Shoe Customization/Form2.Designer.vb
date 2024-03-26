fb296a98bb106dc018f1b564a8804edd8c379190
Evan Henrickson <ewhen283@learner.dvusd.org>
2024-03-26 14:02:09 -0700

    Added shoe size customizer and shoe cleaner options

--- Shoe Customization/Shoe Customization/Form2.Designer.vb
+++ Shoe Customization/Shoe Customization/Form2.Designer.vb
@@ -22,16 +22,152 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox()
+        Me.DomainUpDown1 = New System.Windows.Forms.DomainUpDown()
+        Me.CheckBox8 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox10 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox6 = New System.Windows.Forms.CheckBox()
         Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(142, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Shoe Add Ons"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(32, 110)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(90, 20)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Shoe Size"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(459, 110)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(138, 20)
+        Me.CheckBox2.TabIndex = 2
+        Me.CheckBox2.Text = "Cleaning Supplies"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(238, 110)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(105, 20)
+        Me.CheckBox3.TabIndex = 3
+        Me.CheckBox3.Text = "Custom Sole"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(72, 136)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(55, 20)
+        Me.CheckBox4.TabIndex = 4
+        Me.CheckBox4.Text = "Men"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        Me.CheckBox4.Visible = False
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.Location = New System.Drawing.Point(72, 162)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(76, 20)
+        Me.CheckBox5.TabIndex = 5
+        Me.CheckBox5.Text = "Woman"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        Me.CheckBox5.Visible = False
+        '
+        'DomainUpDown1
+        '
+        Me.DomainUpDown1.Location = New System.Drawing.Point(72, 188)
+        Me.DomainUpDown1.Name = "DomainUpDown1"
+        Me.DomainUpDown1.Size = New System.Drawing.Size(50, 22)
+        Me.DomainUpDown1.TabIndex = 6
+        Me.DomainUpDown1.Text = "0"
+        Me.DomainUpDown1.Visible = False
+        '
+        'CheckBox8
+        '
+        Me.CheckBox8.AutoSize = True
+        Me.CheckBox8.Location = New System.Drawing.Point(502, 136)
+        Me.CheckBox8.Name = "CheckBox8"
+        Me.CheckBox8.Size = New System.Drawing.Size(134, 20)
+        Me.CheckBox8.TabIndex = 9
+        Me.CheckBox8.Text = "Alternate Brushes"
+        Me.CheckBox8.UseVisualStyleBackColor = True
+        Me.CheckBox8.Visible = False
+        '
+        'CheckBox10
+        '
+        Me.CheckBox10.AutoSize = True
+        Me.CheckBox10.Location = New System.Drawing.Point(502, 162)
+        Me.CheckBox10.Name = "CheckBox10"
+        Me.CheckBox10.Size = New System.Drawing.Size(128, 20)
+        Me.CheckBox10.TabIndex = 11
+        Me.CheckBox10.Text = "Microfiber Towel"
+        Me.CheckBox10.UseVisualStyleBackColor = True
+        Me.CheckBox10.Visible = False
+        '
+        'CheckBox6
+        '
+        Me.CheckBox6.AutoSize = True
+        Me.CheckBox6.Location = New System.Drawing.Point(502, 189)
+        Me.CheckBox6.Name = "CheckBox6"
+        Me.CheckBox6.Size = New System.Drawing.Size(78, 20)
+        Me.CheckBox6.TabIndex = 12
+        Me.CheckBox6.Text = "Polisher"
+        Me.CheckBox6.UseVisualStyleBackColor = True
+        Me.CheckBox6.Visible = False
         '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.CheckBox6)
+        Me.Controls.Add(Me.CheckBox10)
+        Me.Controls.Add(Me.CheckBox8)
+        Me.Controls.Add(Me.DomainUpDown1)
+        Me.Controls.Add(Me.CheckBox5)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label1)
         Me.Name = "Form2"
         Me.Text = "Shoe Add Ons"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents DomainUpDown1 As DomainUpDown
+    Friend WithEvents CheckBox8 As CheckBox
+    Friend WithEvents CheckBox10 As CheckBox
+    Friend WithEvents CheckBox6 As CheckBox
 End Class
--- Shoe Customization/Shoe Customization/Form2.vb
+++ Shoe Customization/Shoe Customization/Form2.vb
@@ -1,3 +1,35 @@
 ﻿Public Class Form2
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            CheckBox4.Visible = True
+            CheckBox5.Visible = True
+        Else
+            CheckBox4.Visible = False
+            CheckBox5.Visible = False
+        End If
+    End Sub
 
+    Private Sub CheckBox4_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox4.CheckedChanged
+        If CheckBox4.Checked Then
+            DomainUpDown1.Visible = True
+        Else
+            DomainUpDown1.Visible = False
+        End If
+    End Sub
+
+    Private Sub CheckBox5_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox5.CheckedChanged
+        If CheckBox5.Checked Then
+            DomainUpDown1.Visible = True
+        Else
+            DomainUpDown1.Visible = False
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked Then
+            CheckBox8.Visible = True
+            CheckBox10.Visible = True
+            CheckBox6.Visible = True
+        End If
+    End Sub
 End Class
