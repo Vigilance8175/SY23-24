b08be20a07386198936568a24a3c78d475141ccb
Austin Axlund <amaxl934@learner.dvusd.org>
2024-03-28 13:35:08 -0700

    Added Like Code

--- Shoe Customization/Shoe Customization/Form3.vb
+++ Shoe Customization/Shoe Customization/Form3.vb
@@ -1,3 +1,31 @@
 ﻿Public Class Form3
-
+    Dim correct As Integer
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If TextBox1.Text Like "#### #### #### ####" Then
+            correct += 1
+        Else
+            correct = correct
+        End If
+    End Sub
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        If TextBox2.Text Like "##/##" Then
+            correct += 1
+        Else
+            correct = correct
+        End If
+    End Sub
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+        If TextBox3.Text Like "###" Then
+            correct += 1
+        Else
+            correct = correct
+        End If
+    End Sub
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+        If TextBox4.Text Like "#####" Then
+            correct += 1
+        Else
+            correct = correct
+        End If
+    End Sub
 End Class
