253a619c87315865f17fb3130443ba176bcf1ba1
Keelan Wheeler <kgwhe280@learner.dvusd.org>
2024-04-03 13:27:19 -0700

    AddedIntroFormBtn

--- Shoe Customization/Shoe Customization/Form4.Designer.vb
+++ Shoe Customization/Shoe Customization/Form4.Designer.vb
@@ -22,16 +22,30 @@ Partial Class Form4
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Nextbtn = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Nextbtn
+        '
+        Me.Nextbtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Nextbtn.Location = New System.Drawing.Point(308, 344)
+        Me.Nextbtn.Name = "Nextbtn"
+        Me.Nextbtn.Size = New System.Drawing.Size(185, 94)
+        Me.Nextbtn.TabIndex = 0
+        Me.Nextbtn.Text = "Continue"
+        Me.Nextbtn.UseVisualStyleBackColor = False
         '
         'Form4
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Nextbtn)
         Me.Name = "Form4"
         Me.Text = "Home"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents Nextbtn As Button
 End Class
--- Shoe Customization/Shoe Customization/Form4.vb
+++ Shoe Customization/Shoe Customization/Form4.vb
@@ -1,3 +1,10 @@
-﻿Public Class Form4
+﻿Imports System.Security
 
+Public Class Form4
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Nextbtn.Click
+        Me.Hide()
+        Dim f1 As New Form1
+        f1.ShowDialog()
+        Me.Show()
+    End Sub
 End Class
